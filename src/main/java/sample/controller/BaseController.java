a6a2a86a02e3f1a1f012e4511911d1841ab95ae2
xiaohun's PC <xiaohunsdt@gmail.com>
2019-06-06 14:39:40 +0900

    init framework

---
+++ src/main/java/sample/controller/BaseController.java
@@ -0,0 +1,14 @@
+package sample.controller;
+
+import javafx.fxml.Initializable;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author xiaohun
+ * Date: 2019/6/6
+ * Time: 0:51
+ * Description: TODO
+ */
+public interface BaseController extends Initializable {
+}
---
+++ src/main/java/sample/controller/MainController.java
@@ -0,0 +1,41 @@
+package sample.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class MainController implements BaseController {
+
+    @FXML
+    private Button showTime;
+
+    @FXML
+    private TextField timeField;
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    // When user click on myButton
+    // this method will be called.
+    public void showDateTime(ActionEvent event) {
+        System.out.println("Button Clicked!");
+
+        Date now= new Date();
+
+        DateFormat df = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+        String dateTimeString = df.format(now);
+        // Show in VIEW
+        timeField.setText(dateTimeString);
+    }
+
+
+}
