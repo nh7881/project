2aefba4fdcadcee36e3dfe08dfa3838f9a6501a1
nh7881 <nh7881@naver.com>
2019-06-08 06:40:22 +0900

    Update GameBoardController.java

--- src/main/java/yut/controller/GameBoardController.java
+++ src/main/java/yut/controller/GameBoardController.java
@@ -30,15 +30,16 @@ public class GameBoardController implements BaseController {
      * <p>
      * 10    9    8    7    6    5
      * <p>
-     * 11   20             21    4
+     * 11   71             51    4
      * <p>
      * 12       22     23        3
+     * 12       72     52        3
      * <p>
-     * 13          24            2
+     * 13          73            2
      * <p>
-     * 14     25         26      1
+     * 14     53         74      1
      * <p>
-     * 27                 28
+     *     54                 75
      * <p>
      * 15   16   17   18    19   0
      */
@@ -69,7 +70,7 @@ public class GameBoardController implements BaseController {
                 if (marker != null) {
                     if(checkForward(ContextUtil.getCurrentMarker(), Integer.parseInt(imageView.getId()))){
                         //eat other player's marker, make it go back user marker bar
-                        this.mainController.getUserBoardController().addMarkerBtn(marker);
+                        //this.mainController.getUserBoardController().addMarkerBtn(marker);
                     }else {
                         //set this marker is current marker, forward form here
                         ContextUtil.setCurrentMarker(marker);
@@ -123,37 +124,119 @@ public class GameBoardController implements BaseController {
                 AnchorPane.setBottomAnchor(gameGrid, 10.0);
                 AnchorPane.setLeftAnchor(gameGrid, (gridId - 15) * 83.0 + 10.0);
             } else if (gridId == 20) {
+                gameGrid.setId("71");
                 AnchorPane.setTopAnchor(gameGrid, 100.0);
                 AnchorPane.setLeftAnchor(gameGrid, 90.0);
             } else if (gridId == 21) {
-                AnchorPane.setTopAnchor(gameGrid, 100.0);
-                AnchorPane.setRightAnchor(gameGrid, 90.0);
-            } else if (gridId == 22) {
+                gameGrid.setId("72");
                 AnchorPane.setTopAnchor(gameGrid, 160.0);
                 AnchorPane.setLeftAnchor(gameGrid, 150.0);
-            } else if (gridId == 23) {
-                AnchorPane.setTopAnchor(gameGrid, 160.0);
-                AnchorPane.setRightAnchor(gameGrid, 150.0);
-            } else if (gridId == 24) {
+            } else if (gridId == 22) {
+                gameGrid.setId("73");
                 AnchorPane.setTopAnchor(gameGrid, 220.0);
                 AnchorPane.setRightAnchor(gameGrid, 210.0);
-            } else if (gridId == 25) {
-                AnchorPane.setTopAnchor(gameGrid, 280.0);
-                AnchorPane.setLeftAnchor(gameGrid, 150.0);
-            } else if (gridId == 26) {
+            } else if (gridId == 23) {
+                gameGrid.setId("74");
                 AnchorPane.setTopAnchor(gameGrid, 280.0);
                 AnchorPane.setRightAnchor(gameGrid, 150.0);
-            } else if (gridId == 27) {
-                AnchorPane.setTopAnchor(gameGrid, 340.0);
-                AnchorPane.setLeftAnchor(gameGrid, 90.0);
-            } else if (gridId == 28) {
+            } else if (gridId == 24) {
+                gameGrid.setId("75");
                 AnchorPane.setTopAnchor(gameGrid, 340.0);
                 AnchorPane.setRightAnchor(gameGrid, 90.0);
+            } else if (gridId == 25) {
+                gameGrid.setId("51");
+                AnchorPane.setTopAnchor(gameGrid, 100.0);
+                AnchorPane.setRightAnchor(gameGrid, 90.0);
+            } else if (gridId == 26) {
+                gameGrid.setId("52");
+                AnchorPane.setTopAnchor(gameGrid, 160.0);
+                AnchorPane.setRightAnchor(gameGrid, 150.0);
+            } else if (gridId == 27) {
+                gameGrid.setId("54");
+                AnchorPane.setTopAnchor(gameGrid, 280.0);
+                AnchorPane.setLeftAnchor(gameGrid, 150.0);
+            } else if (gridId == 28) {
+                gameGrid.setId("55");
+                AnchorPane.setTopAnchor(gameGrid, 340.0);
+                AnchorPane.setLeftAnchor(gameGrid, 90.0);
             }
 
             //set road sign image
             this.setRoadSignImage(gameGrid);
         });
+    }
+
+    // before mark move, position where mark will go change.
+    public int changeMarker(int index) {
+        if(index == 5) {
+            return -45; //first diagonal
+        }
+        else if(index == 10) {
+            return -60; //second diagonal
+        }
+        return 0;
+    }
+    // after mark move, position where mark gone change.
+    public int changeTarget(int markerIndex, int targetIndex){
+        if(markerIndex > 50 && markerIndex < 56 && targetIndex >14 && targetIndex < 20) {
+            return 41; // third diagonal
+        }
+        else if((markerIndex < 53 && markerIndex > 50 && targetIndex == 73) ||
+                (markerIndex == 5 && targetIndex == 73)) {
+            return -20; //center
+        }
+        else if((markerIndex == 51 && targetIndex == 5) ||
+                (markerIndex == 5 && targetIndex == 4)) {
+            return 45;
+        }
+        else if((markerIndex == 71 && targetIndex == 10) ||
+                (markerIndex == 10 && targetIndex == 9)) {
+            return 60;
+        }
+        else if(markerIndex == 0 && targetIndex == 75) {
+            return -74;
+        }
+        else if(markerIndex == 54 && targetIndex == 73) {
+            return -20;
+        }
+        else if(markerIndex < 20  && markerIndex >= 15  &&targetIndex == 0) {
+            return 20;
+        }
+        else if(markerIndex > 70 && targetIndex == 0) {
+            return  76;
+        }
+        return 0;
+    }
+    // check marker finish
+    public int checkMarkerEnd(int targetid, int stepCount) {
+        int i = 0;
+        if (targetid == 0) {
+            for(i = 0;
+                i <this.mainController.getUserBoardController().getAllScore().size();
+                i++ ) {
+                if (stepCount <
+                        this.mainController.getUserBoardController().getAllScore().get(i))
+                {
+                    return this.mainController.getUserBoardController().getAllScore().get(i);
+                }
+            }
+        }
+        return stepCount;
+    }
+
+    // change grid id and index
+    public int changeDialog(int index){
+        int temp = index;
+        if(index > 70 && index <= 75) {
+            temp = temp - 51;
+        }
+        else if(index == 51 || index == 52) {
+            temp = temp - 26;
+        }
+        else if(index == 54 || index == 55) {
+            temp = temp - 27;
+        }
+        return temp;
     }
 
     /**
@@ -165,26 +248,37 @@ public class GameBoardController implements BaseController {
     public void forWard(Marker marker, int targetIndex) {
         int stepCount = targetIndex - marker.getIndex();
 
+        // before move marker, change position
+        stepCount = stepCount + changeMarker(marker.getIndex());
+        // after move marker, change position
+        stepCount = stepCount + changeTarget(marker.getIndex(), targetIndex);
         //can forward target
-        ImageView start = gameGridList.get(marker.getIndex());
-        ImageView target = gameGridList.get(targetIndex);
-
+        ImageView start = gameGridList.get(changeDialog(marker.getIndex()));
+        ImageView target = gameGridList.get(changeDialog(targetIndex));
         //reset grid image
         start.setImage(new Image(YutGameApp.class.getResourceAsStream("/res/blue_marker.png")));
         this.setRoadSignImage(start);
 
-        //forward
-        String playerAnimal = SettingUtil.getProperty("player" + marker.getOwnPlayer().getId() + "Animal");
-        target.setImage(new Image(YutGameApp.class.getResourceAsStream("/res/marker/" + playerAnimal + ".png")));
-        target.setRotate(0);
+        //check marker end
+        if(checkMarkerEnd(targetIndex, stepCount) != stepCount ) { // when marker finish, discard first score.
+            stepCount = checkMarkerEnd(targetIndex, stepCount);
+            //if marker fade out, other source error, help me......sorry.....
+            //this.mainController.getUserBoardController().removeMarkerBtn(ContextUtil.getCurrentMarker());
+        }
+        else {
+            //forward
+            String playerAnimal = SettingUtil.getProperty("player" + marker.getOwnPlayer().getId() + "Animal");
+            target.setImage(new Image(YutGameApp.class.getResourceAsStream("/res/marker/" + playerAnimal + ".png")));
+            target.setRotate(0);
 
-        //save all data
-        target.setUserData(marker);
-        marker.setIndex(targetIndex);
+            //save all data
+            target.setUserData(marker);
+            marker.setIndex(targetIndex);
+        }
+            //delete button in user marker bar
+            this.mainController.getUserBoardController().removeMarkerBtn(ContextUtil.getCurrentMarker());
+            this.mainController.getUserBoardController().removeScoreImageView(stepCount);
 
-        //delete button in user marker bar
-        this.mainController.getUserBoardController().removeMarkerBtn(ContextUtil.getCurrentMarker());
-        this.mainController.getUserBoardController().removeScoreImageView(stepCount);
     }
 
     public boolean checkForward(Marker marker, int targetIndex){
@@ -194,14 +288,21 @@ public class GameBoardController implements BaseController {
 
         List<Integer> allScores = this.mainController.getUserBoardController().getAllScore();
         int stepCount = targetIndex - marker.getIndex();
-
+        stepCount = stepCount + changeMarker(marker.getIndex());
+        // after move marker, change position
+        stepCount = stepCount + changeTarget(marker.getIndex(), targetIndex);
+        //check marker finish
+        if(checkMarkerEnd(targetIndex, stepCount) != stepCount) {
+            return true;
+        }
+        //can forward target
         return allScores.contains(stepCount);
     }
 
     private void setRoadSignImage(ImageView gameGrid) {
         int gridId = Integer.parseInt(gameGrid.getId());
 
-        if (gridId == 0 || gridId == 5 || gridId == 10 || gridId == 15 || gridId == 24) {
+        if (gridId == 0 || gridId == 5 || gridId == 10 || gridId == 15 || gridId == 73) {
             gameGrid.setImage(new Image(YutGameApp.class.getResourceAsStream("/res/arrow.png")));
             switch (gridId) {
                 case 0:
@@ -213,9 +314,10 @@ public class GameBoardController implements BaseController {
                 case 10:
                     gameGrid.setRotate(45);
                     break;
-                case 24:
+                case 73:
                     gameGrid.setRotate(45);
                     break;
+
             }
         }
     }
