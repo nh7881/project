ad96e30a42aea9f5178c4645720c37b6dd42e127
nh7881 <nh7881@naver.com>
2019-06-07 12:37:56 +0900

    create yut

---
+++ src/main/java/yut/model/Yut.java
@@ -0,0 +1,12 @@
+package main.java.yut.model;
+
+public class Yut {
+	
+	public boolean isback;
+	public boolean back_do;	
+	
+	public Yut( boolean back_do) {
+		this.isback = false;
+		this.back_do = back_do;
+	}
+}
